0dc50297ab90dace6d651fb45230ad4ae115c2f8
gitlab-runner <do-not-reply@cypress.com>
2026-03-26 19:39:30 -0500

    Upload mtb-example-psoc4-direction-detection-using-quadrature-decoder [4724]

--- main.c
+++ main.c
@@ -1,43 +1,40 @@
 /******************************************************************************
  * File Name:   main.c
  *
- * Description: This is the source code for the PSoC 4 Direction Detection
+ * Description: This is the source code for the PSOC 4 Direction Detection
  * using Quadrature Decoder application for ModusToolbox.
  *
  * Related Document: See README.md
  *
  *
  *******************************************************************************
- * Copyright 2023-2025, Cypress Semiconductor Corporation (an Infineon company) or
- * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+ * (c) 2023-2026, Infineon Technologies AG, or an affiliate of Infineon
+ * Technologies AG. All rights reserved.
+ * This software, associated documentation and materials ("Software") is
+ * owned by Infineon Technologies AG or one of its affiliates ("Infineon")
+ * and is protected by and subject to worldwide patent protection, worldwide
+ * copyright laws, and international treaty provisions. Therefore, you may use
+ * this Software only as provided in the license agreement accompanying the
+ * software package from which you obtained this Software. If no license
+ * agreement applies, then any use, reproduction, modification, translation, or
+ * compilation of this Software is prohibited without the express written
+ * permission of Infineon.
  *
- * This software, including source code, documentation and related
- * materials ("Software") is owned by Cypress Semiconductor Corporation
- * or one of its affiliates ("Cypress") and is protected by and subject to
- * worldwide patent protection (United States and foreign),
- * United States copyright laws and international treaty provisions.
- * Therefore, you may use this Software only as provided in the license
- * agreement accompanying the software package from which you
- * obtained this Software ("EULA").
- * If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
- * non-transferable license to copy, modify, and compile the Software
- * source code solely for use in connection with Cypress's
- * integrated circuit products.  Any reproduction, modification, translation,
- * compilation, or representation of this Software except as specified
- * above is prohibited without the express written permission of Cypress.
- *
- * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
- * reserves the right to make changes to the Software without notice. Cypress
- * does not assume any liability arising out of the application or use of the
- * Software or any product or circuit described in the Software. Cypress does
- * not authorize its products for use in any products where a malfunction or
- * failure of the Cypress product may reasonably be expected to result in
- * significant property damage, injury or death ("High Risk Product"). By
- * including Cypress's product in a High Risk Product, the manufacturer
- * of such system or application assumes all risk of such use and in doing
- * so agrees to indemnify Cypress against all liability.
+ * Disclaimer: UNLESS OTHERWISE EXPRESSLY AGREED WITH INFINEON, THIS SOFTWARE
+ * IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING, BUT NOT LIMITED TO, ALL WARRANTIES OF NON-INFRINGEMENT OF
+ * THIRD-PARTY RIGHTS AND IMPLIED WARRANTIES SUCH AS WARRANTIES OF FITNESS FOR A
+ * SPECIFIC USE/PURPOSE OR MERCHANTABILITY.
+ * Infineon reserves the right to make changes to the Software without notice.
+ * You are responsible for properly designing, programming, and testing the
+ * functionality and safety of your intended application of the Software, as
+ * well as complying with any legal requirements related to its use. Infineon
+ * does not guarantee that the Software will be free from intrusion, data theft
+ * or loss, or other breaches ("Security Breaches"), and Infineon shall have
+ * no liability arising out of any Security Breaches. Unless otherwise
+ * explicitly approved by Infineon, the Software may not be used in any
+ * application where a failure of the Product or any consequences of the use
+ * thereof can reasonably be expected to result in personal injury.
  *******************************************************************************/
 
 /*******************************************************************************
@@ -50,13 +47,16 @@
 /*******************************************************************************
  * Macros
  ********************************************************************************/
-#define MID_COUNT           (0x8000u)   /* for 16 bit counter*/
-#define DELAY               (500u)      /* 500 msec */
-                                        /* Delay should be more than the period
-                                        of input signal to the quadrature
-                                        decoder */
-#define ROTATION    1  /* CLOCKWISE - 1, ANTI_CLOCKWISE - 2, NO_ROTATION - 0 */
-#define TCPWM_PWM_VAL       (999UL)     /* Counter value*/
+#define MID_COUNT         (0x8000u)     /* for 16 bit counter */
+#define DELAY_500MS       (500u)        /* 500 msec
+                                         * Delay should be more than the period of
+                                         * input signal to the quadrature decoder
+                                         */
+#define CLOCKWISE         (1u)
+#define COUNTER_CLOCKWISE (2u)
+#define NO_ROTATION       (0u)
+#define ROTATION          (CLOCKWISE)   /* Rotation direction */
+#define TCPWM_PWM_VAL     (999UL)       /* Counter value*/
 
 /*******************************************************************************
  * Function Name: main
@@ -79,14 +79,14 @@ int main(void)
     cy_rslt_t result;
 
     /* Initialize the device and board peripherals */
-    result = cybsp_init() ;
+    result = cybsp_init();
     if (result != CY_RSLT_SUCCESS)
     {
         CY_ASSERT(0);
     }
 
-    uint16_t count      = MID_COUNT;
-    uint16_t count_prev  = MID_COUNT;
+    uint16_t count = MID_COUNT;
+    uint16_t count_prev = MID_COUNT;
 
     /* Enable global interrupts */
     __enable_irq();
@@ -109,20 +109,20 @@ int main(void)
      * will emulate 90 degrees phase shift.
      */
 
-#if (ROTATION == 1)
+#if (ROTATION == CLOCKWISE)
 
     /* Set the counter value to generate phase shift */
-    Cy_TCPWM_PWM_SetCounter(PWM_phiA_HW,PWM_phiA_NUM,TCPWM_PWM_VAL);
+    Cy_TCPWM_PWM_SetCounter(PWM_phiA_HW, PWM_phiA_NUM, TCPWM_PWM_VAL);
     /* Trigger a software start on the TCPWM instance. This is required when
      * no other hardware input signal is connected to the peripheral to act as
      * a trigger source.
      */
     Cy_TCPWM_TriggerStart(PWM_phiA_HW, PWM_phiA_MASK | PWM_phiB_MASK);
 
-#elif (ROTATION == 2)
+#elif (ROTATION == COUNTER_CLOCKWISE)
 
     /* Set the counter value to generate phase shift */
-    Cy_TCPWM_PWM_SetCounter(PWM_phiB_HW,PWM_phiB_NUM,TCPWM_PWM_VAL);
+    Cy_TCPWM_PWM_SetCounter(PWM_phiB_HW, PWM_phiB_NUM, TCPWM_PWM_VAL);
     /* Trigger a software start on the TCPWM instance. This is required when
      * no other hardware input signal is connected to the peripheral to act as
      * a trigger source.
@@ -131,47 +131,34 @@ int main(void)
 
 #endif
 
-    for(;;)
+    for (;;)
     {
         count = Cy_TCPWM_QuadDec_GetCounter(QuadDec_HW, QuadDec_NUM);
-    /* For clockwise rotation count value increases and for anti-clockwise
-     * counter value decreases.
-     */
-#if defined (CY_DEVICE_PSOC4HV144K)
-        if(count > count_prev)       /* Condition for clockwise rotation */
+        /* For clockwise rotation count value increases and for counter-clockwise
+         * counter value decreases.
+         */
+
+        /* Correlation between LED1/LED2 state and rotation direction is
+         * explained in README.md #Operation part
+         */
+        if (count > count_prev) /* Condition for clockwise rotation */
         {
-            Cy_GPIO_Set(CYBSP_LED7_PORT, CYBSP_LED7_NUM);  /* LED  OFF */
-            Cy_SysLib_Delay(DELAY);
+            Cy_GPIO_Clr(LED1_PORT, LED1_NUM);
+            Cy_GPIO_Set(LED2_PORT, LED2_NUM);
+            Cy_SysLib_Delay(DELAY_500MS);
         }
-        else if(count < count_prev)  /* Condition for anti-clockwise rotation */
+        else if (count < count_prev) /* Condition for counter-clockwise rotation */
         {
-            Cy_GPIO_Clr(CYBSP_LED7_PORT, CYBSP_LED7_NUM);  /* LED  ON  */
-            Cy_SysLib_Delay(DELAY);
+            Cy_GPIO_Set(LED1_PORT, LED1_NUM);
+            Cy_GPIO_Clr(LED2_PORT, LED2_NUM);
+            Cy_SysLib_Delay(DELAY_500MS);
         }
-        else                        /* No rotation */
+        else /* No rotation */
         {
-            Cy_GPIO_Inv(CYBSP_LED7_PORT, CYBSP_LED7_NUM);  /* LED  OFF */
-            Cy_SysLib_Delay(DELAY);
+            Cy_GPIO_Set(LED1_PORT, LED1_NUM);
+            Cy_GPIO_Set(LED2_PORT, LED2_NUM);
         }
-#else
-        if(count > count_prev)       /* Condition for clockwise rotation */
-        {
-            Cy_GPIO_Clr(LED1_PORT, LED1_NUM);   /* LED 1 ON  */
-            Cy_GPIO_Set(LED2_PORT, LED2_NUM);  /* LED 2 OFF */
-            Cy_SysLib_Delay(DELAY);
-        }
-        else if(count < count_prev)  /* Condition for anti-clockwise rotation */
-        {
-            Cy_GPIO_Set(LED1_PORT, LED1_NUM);   /* LED 1 OFF */
-            Cy_GPIO_Clr(LED2_PORT, LED2_NUM);  /* LED 2 ON  */
-            Cy_SysLib_Delay(DELAY);
-        }
-        else                        /* No rotation */
-        {
-            Cy_GPIO_Set(LED1_PORT, LED1_NUM);   /* LED 1 OFF */
-            Cy_GPIO_Set(LED2_PORT, LED2_NUM);  /* LED 2 OFF */
-        }
-#endif
+
         /* Update count_prev value */
         count_prev = count;
     }
